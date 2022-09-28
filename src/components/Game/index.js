b50a5ecc7870bce62039aa051544a111c5b4270d
USR\juliana.santa <juliana.santa@fleetcor.com.br>
2022-09-28 02:10:28 -0300

    style: game component

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -2,26 +2,26 @@ import style from './style.module.css'
 
 const Game = ({verifyLetter}) => {
   return (
-    <div className="game">
-      <p className="points">
+    <div className={style.game}>
+      <p className={style.points}>
         <span>Pontuaçõa: 000</span>
       </p>
-      <h1>Adivinh a palavra:</h1>
-      <h3 className="tip">
+      <h1>Adivinhe a palavra:</h1>
+      <h3 className={style.tip}>
         Dica sobre a palavra: <span>Dica...</span>
       </h3>
-      <div className="wordContainer">
-        <span className="letter">A</span>
-        <span className="blankSquare"></span>
+      <div className={style.wordContainer}>
+        <span className={style.letter}>A</span>
+        <span className={style.blankSquare}></span>
       </div>
-      <div className="letterContainer">
+      <div className={style.letterContainer}>
         <p>Tente adivinhar uam letra da palavra:</p>
         <form>
           <input type="text" name="letter" maxLength={1} required />
           <button onClick={verifyLetter}>Jogar</button>
         </form>
       </div>
-      <div className="wrongLettersContaier">
+      <div className={style.wrongLettersContaier}>
         <p>Letras já utilizadas:</p>
         <span>a,</span>
         <span>b,</span>
