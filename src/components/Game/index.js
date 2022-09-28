e7dbfb31a1c9fd18ce2280399f786ca08faabc55
USR\juliana.santa <juliana.santa@fleetcor.com.br>
2022-09-28 01:58:06 -0300

    feat: structure game component

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -2,9 +2,30 @@ import style from './style.module.css'
 
 const Game = ({verifyLetter}) => {
   return (
-    <div>
-      <h1>Game</h1>
-      <button onClick={verifyLetter}>Finalizar Jogo</button>
+    <div className="game">
+      <p className="points">
+        <span>Pontuaçõa: 000</span>
+      </p>
+      <h1>Adivinh a palavra:</h1>
+      <h3 className="tip">
+        Dica sobre a palavra: <span>Dica...</span>
+      </h3>
+      <div className="wordContainer">
+        <span className="letter">A</span>
+        <span className="blankSquare"></span>
+      </div>
+      <div className="letterContainer">
+        <p>Tente adivinhar uam letra da palavra:</p>
+        <form>
+          <input type="text" name="letter" maxLength={1} required />
+          <button onClick={verifyLetter}>Jogar</button>
+        </form>
+      </div>
+      <div className="wrongLettersContaier">
+        <p>Letras já utilizadas:</p>
+        <span>a,</span>
+        <span>b,</span>
+      </div>
     </div>
   )
 }
